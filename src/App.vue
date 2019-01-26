69c5899a3a0287c6029ad7d4cc4fe13a8330b5a9
Steve Kulesza <steve.kulesza@moxieusa.com>
2019-01-26 12:50:23 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" class="full">
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  $font-base:  "Raleway", sans-serif;
+  $font-about:  "Droid Serif", sans-serif;
+
+  $dark-grey:   #222426;
+  $middle-blue: #00f6ed;
+  $light-white: #fdfdff;
+  $light-blue:  #edf9f9;
+  $dark-blue:  #546a7b;
+  $sidebar-color: rgba($dark-blue, .95);
+  $sidebar-item-color: rgba($dark-blue, 0);
+
+  $phone: "(min-width: 320px) and (max-width: 767px)";
+  $tablet: "(min-width: 768px) and (max-width: 1023px)";
+  $desktop: "(min-width: 1024px)";
+
+  html,
+  body {
+    font-family: $font-base;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-tap-highlight-color: transparent;
+    height: 100%;
+    min-height: 100%;
+    margin:-21px 0 0;    
   }
-}
+  
+  a {
+    color: inherit;
+    text-decoration: none !important;
+  }
+  .text-center {
+    text-align: center;
+  }
+  .full {
+    margin:0;
+    height: 100%;
+    min-height: 100%;
+  }
+  .center {
+    left: 50%;
+    position: relative;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .container-fluid {
+    min-height: 100%;
+    padding: 0;
+  }
 </style>
---
+++ src/components/Intro.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="full">
+    <div id="home" class="container-fluid full">
+      <h1 class="center text-center">
+        <p>Hi, I'm <strong class="dark-gray-font">Steven Kulesza</strong></p>
+        <vue-typed-js class="text-center" :backSpeed="20" :showCursor="false" :loop="true" :strings="['a full stack developer', 'a problem solver', 'a javascript addict']">
+          <p class="text-center">and I am <span class="typing text-center"></span>.</p>
+        </vue-typed-js>
+        <p>Living in <strong class="dark-gray-font">Atlanta</strong> and working at <strong class="dark-gray-font"><a href="https://moxieusa.com/">Moxie</a></strong>.</p>
+      </h1>
+      <footer class='footer text-center'>
+        <a target='_blank' href='../assets/resume-2019.pdf'>resume</a> - <a href='mailto:info@stevenkulesza.com'>email</a>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import VueTypedJs from 'vue-typed-js';
+
+Vue.use(VueTypedJs);
+
+@Component
+export default class Intro extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss">
+  $font-base:  "Raleway", sans-serif;
+  $font-about:  "Droid Serif", sans-serif;
+
+  $dark-grey:   #222426;
+
+  $phone: "(min-width: 320px) and (max-width: 767px)";
+  $tablet: "(min-width: 768px) and (max-width: 1023px)";
+  $desktop: "(min-width: 1024px)";
+  
+@media #{$desktop} {
+  #home {
+    h1 {
+
+      p {
+        font-size: 42px;
+        font-weight: 400;
+      }
+
+      strong {
+        font-size: 46px;
+      }
+    }
+  }
+}
+
+@media #{$tablet} {
+  #home {
+    h1 {
+
+      p {
+        font-size: 38px;
+        font-weight: 400;
+      }
+
+      strong {
+        font-size: 42px;
+      }
+    }
+  }
+}
+
+@media #{$phone} {
+  #home {
+    h1 {
+
+      p {
+        font-size: 30px;
+        font-weight: 400;
+      }
+
+      strong {
+        font-size: 34px;
+      }
+    }
+  }
+}
+.typed-element{
+  display: block;
+}
+.dark-gray-background {
+  background-color: $dark-grey;
+}
+
+.dark-gray-font {
+  color: $dark-grey !important;
+}
+
+.footer {
+    bottom: 30px;
+    position: absolute;
+    width: 100%;
+}
+</style>
